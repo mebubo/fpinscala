be34aa3017c2928b8ba08318d9b6e607c4f7170f
Sergei Dolgov <dolgovs@gmail.com>
2017-03-04 11:00:34 +0100

    Another attempt with State

---
+++ exercises/src/main/scala/fpinscala/state2/State.scala
@@ -0,0 +1,53 @@
+package fpinscala.state2
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+object RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, nextRNG) = rng.nextInt
+    if (i < 0) {
+      (-(i+1), nextRNG)
+    } else {
+      (i, nextRNG)
+    }
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, nextRNG) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), nextRNG)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng1) = rng.nextInt
+    val (d, rng2) = double(rng)
+    ((i, d), rng2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, rng1) = double(rng)
+    val (i, rng2) = rng1.nextInt
+    ((d, i), rng2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
+  }
+}
+
+
