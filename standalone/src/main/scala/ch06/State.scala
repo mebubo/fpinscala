8dfdb3f0f76a6ce078c9d985c60abe862d0ee2e3
Sergei Dolgov <dolgovs@gmail.com>
2017-03-15 08:13:29 +0100

    6.5-6.9

--- standalone/src/main/scala/ch06/State.scala
+++ standalone/src/main/scala/ch06/State.scala
@@ -21,7 +21,7 @@ object RNG {
 
   def double(rng: RNG): (Double, RNG) = {
     val (i, rng2) = nonNegativeInt(rng)
-    (i / (i.toDouble + 1), rng2)
+    (i / (Int.MaxValue.toDouble + 1), rng2)
   }
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
@@ -54,4 +54,64 @@ object RNG {
     }
     go(count, rng, List())
   }
+
+  type Rand[A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = r => (a, r)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    r => {
+      val (a, r2) = s(r)
+      (f(a), r2)
+    }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  def double2: Rand[Double] = map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng)
+    (f(a, b), rng3)
+  }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def intDouble2: Rand[(Int, Double)] = both(int, double)
+
+  def doubleInt2: Rand[(Double, Int)] = both(double, int)
+
+  def sequence[A](xs: List[Rand[A]]): Rand[List[A]] =
+    xs.foldRight(unit(List[A]()))((x, r) => map2(x, r)(_ :: _))
+
+  def flatMap[A, B](ra: Rand[A])(f: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val rb = f(a)
+      rb(rng2)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(int)(i => {
+      val max = (Int.MaxValue / n) * n
+      if (i > max) nonNegativeLessThan(n)
+      else unit(i % n)
+    })
+
+  def mapViaFlatMap[A, B](r: Rand[A])(f: A => B): Rand[B] =
+    flatMap(r)(a => unit(f(a)))
+
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+
 }
+
+object Main {
+  def main(args: Array[String]) = {
+    printf(RNG.nonNegativeLessThan(6)(SimpleRNG(1)).toString)
+  }
+}
+
