bd5f9bf76d255facf7edfe5cd0be26ee81ad95c6
Sergei Dolgov <dolgovs@gmail.com>
2017-03-16 17:45:51 +0100

    6.10

--- standalone/src/main/scala/ch06/State.scala
+++ standalone/src/main/scala/ch06/State.scala
@@ -109,6 +109,25 @@ object RNG {
 
 }
 
+case class State[S, A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => State.unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap (a => sb map (b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s2) = run(s)
+    f(a).run(s2)
+  })
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+}
+
 object Main {
   def main(args: Array[String]) = {
     printf(RNG.nonNegativeLessThan(6)(SimpleRNG(1)).toString)
