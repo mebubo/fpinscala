b7d4e46f9d33889e47280c8162b0d86a8dbbc70e
Sergei Dolgov <dolgovs@gmail.com>
2017-03-20 20:52:51 +0100

    8.4-8.5

--- standalone/src/main/scala/ch08/Gen.scala
+++ standalone/src/main/scala/ch08/Gen.scala
@@ -1,10 +1,30 @@
 package ch08
 
+import Prop._
+import ch06.State
+import ch06.RNG
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+}
+
 trait Prop {
-  def check: Boolean
-  def &&(p: Prop): Prop = {
-    new Prop {
-      def check = Prop.this.check && p.check
-    }
-  }
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+}
+
+case class Gen[A](sample: State[RNG, A])
+
+object Gen {
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.map(RNG.nonNegativeLessThan(stopExclusive - start))(_ + start)))
+
+  def unit[A](a: A): Gen[A] =
+    Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.map(RNG.nonNegativeInt)(_ % 2 == 0)))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
 }
