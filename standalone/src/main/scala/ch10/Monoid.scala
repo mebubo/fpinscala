6d18ff1ddf23e5b7199d1b414c8f0558f6ee9f06
Sergei Dolgov <dolgovs@gmail.com>
2017-03-25 10:09:49 +0100

    10.3-10.4

--- standalone/src/main/scala/ch10/Monoid.scala
+++ standalone/src/main/scala/ch10/Monoid.scala
@@ -1,3 +1,5 @@
+package ch10
+
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
@@ -38,6 +40,24 @@ object Monoid {
   def optionMonoid[A] = new Monoid[Option[A]] {
     def op(a1: Option[A], a2: Option[A]) = a1.orElse(a2)
     val zero = None
+  }
+
+  def endoMonoid[A] = new Monoid[A => A] {
+    def op(a1: A => A, a2: A => A): (A => A) = a1 compose a2
+    def zero = x => x
+  }
+
+  import ch08.Prop
+  import ch08.Gen
+
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
+    Prop.forAll(for {
+      x <- gen
+      y <- gen
+      z <- gen
+    } yield (x, y, z)){ case (x, y, z) => m.op(m.op(x, y), z) == m.op(x, m.op(y, z)) }
+    Prop.forAll(gen)((a: A) => m.op(a, m.zero) == a)
+    Prop.forAll(gen)((a: A) => m.op(m.zero, a) == a)
   }
 }
 
