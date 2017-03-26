1d364cf9bc4e9c2f6504bbf3e3de118a7b5d47b3
Sergei Dolgov <dolgovs@gmail.com>
2017-03-26 23:10:26 +0200

    10.10-10.11

--- standalone/src/main/scala/ch10/Monoid.scala
+++ standalone/src/main/scala/ch10/Monoid.scala
@@ -114,5 +114,35 @@ object Monoid {
     }
     foldMapV(v, orderedMonoid)(x => Some(x, x, true)).map(_._3).getOrElse(false)
   }
+
+  sealed trait WC
+  case class Stub(chars: String) extends WC
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
+
+  val wcMonoid = new Monoid[WC] {
+    val zero = Stub("")
+
+    def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      case (Stub(s1), Stub(s2)) => Stub(s1 + s2)
+      case (Stub(s1), Part(l, w, r)) => Part(s1 + l, w, r)
+      case (Part(l, w, r), Stub(s)) => Part(l, w, r + s)
+      case (Part(l1, w1, r1), Part(l2, w2, r2)) =>
+        Part(l1, w1 + (if ((r1 + l2).isEmpty) 0 else 1) + w2, r2)
+    }
+  }
+
+  def count(s: String): Int = {
+    def unstub(x: String): Int = if (x.isEmpty) 0 else 1
+    foldMapV(s.toIndexedSeq, wcMonoid){ c =>
+      if (c.isWhitespace)
+        Part("", 0, "")
+      else
+        Stub(c.toString)
+      } match {
+        case Stub(x) => unstub(x)
+        case Part(l, w, r) => unstub(l) + w + unstub(r)
+      }
+
+  }
 }
 
