c47ef831c072041212d602f8b3598e714b1d906b
Sergei Dolgov <dolgovs@gmail.com>
2017-03-27 09:33:44 +0200

    10.13

--- standalone/src/main/scala/ch10/Monoid.scala
+++ standalone/src/main/scala/ch10/Monoid.scala
@@ -184,3 +184,22 @@ object StreamFoldable extends Foldable[Stream] {
   override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B =
     as.foldLeft(z)(f)
 }
+
+sealed trait Tree[A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object TreeFoldable extends Foldable[Tree] {
+  override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B = as match {
+    case Leaf(a) => f(a, z)
+    case Branch(l, r) => foldRight(l)(foldRight(r)(z)(f))(f)
+  }
+  override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = as match {
+    case Leaf(a) => f(z, a)
+    case Branch(l, r) => foldLeft(r)(foldLeft(r)(z)(f))(f)
+  }
+  override def foldMap[A, B](as: Tree[A])(f: A => B)(m: Monoid[B]): B = as match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => m.op(foldMap(l)(f)(m), foldMap(r)(f)(m))
+  }
+}
