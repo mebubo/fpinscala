351b1cab108e699a728133b053b054a47704e474
Sergei Dolgov <dolgovs@gmail.com>
2017-03-27 09:17:45 +0200

    10.12

--- standalone/src/main/scala/ch10/Monoid.scala
+++ standalone/src/main/scala/ch10/Monoid.scala
@@ -144,5 +144,43 @@ object Monoid {
       }
 
   }
+
 }
 
+trait Foldable[F[_]] {
+  import Monoid._
+
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
+    foldMap(as)(f.curried)(endoMonoid)(z)
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B =
+    foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid))(z)
+  def foldMap[A, B](as: F[A])(f: A => B)(m: Monoid[B]): B =
+    foldRight(as)(m.zero)((a, b) => m.op(f(a), b))
+  def concatenate[A](as: F[A])(m: Monoid[A]): A =
+    foldLeft(as)(m.zero)(m.op)
+}
+
+object ListFoldable extends Foldable[List] {
+  override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
+    as.foldRight(z)(f)
+  override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+  override def foldMap[A, B](as: List[A])(f: A => B)(m: Monoid[B]): B =
+    as.foldLeft(m.zero)((b, a) => m.op(b, f(a)))
+}
+
+object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+  override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B =
+    as.foldRight(z)(f)
+  override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+  override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(m: Monoid[B]): B =
+    Monoid.foldMapV(as, m)(f)
+}
+
+object StreamFoldable extends Foldable[Stream] {
+  override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B =
+    as.foldRight(z)(f)
+  override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+}
