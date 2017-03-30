c2e5e81fb9b249e2562947ef1cae2489128b9dcd
Sergei Dolgov <dolgovs@gmail.com>
2017-03-30 09:05:27 +0200

    11.1, 11.6

--- standalone/src/main/scala/ch11/Monad.scala
+++ standalone/src/main/scala/ch11/Monad.scala
@@ -36,9 +36,25 @@ trait Monad[F[_]] extends Functor[F] {
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
     sequence(List.fill(n)(fa))
 
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((_, _))
+
+  def filterM0[A](la: List[A])(f: A => F[Boolean]): F[List[A]] = {
+    val lfab: List[F[(A, Boolean)]] = la.map(a => map(f(a))(b => (a, b)))
+    map(sequence(lfab))(l => l.filter(_._2).map(_._1))
+  }
+
+  def filterM[A](la: List[A])(f: A => F[Boolean]): F[List[A]] =
+    la.foldRight(unit(List[A]()))((a, fla) => {
+      val fb = f(a)
+      map2(fb, fla)((b, la) => if (b) a :: la else la)
+    })
 }
 
 import ch08.Gen
+import ch07.Par
+import ch07.Par._
+import ch09.Parsers
 
 object Monad {
   val genMonad: Monad[Gen] = new Monad[Gen] {
@@ -46,4 +62,40 @@ object Monad {
     def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] =
       fa.flatMap(f)
   }
+
+  val parMonad: Monad[Par] = new Monad[Par] {
+    def unit[A](a: A): Par[A] = Par.unit(a)
+    def flatMap[A, B](fa: Par[A])(f: A => Par[B]): Par[B] =
+      Par.flatMap(fa)(f)
+  }
+
+  val listMonad: Monad[List] = new Monad[List] {
+    def unit[A](a: A): List[A] = List[A]()
+    def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] =
+      fa.flatMap(f)
+  }
+
+  val optionMonad: Monad[Option] = new Monad[Option] {
+    def unit[A](a: A): Option[A] = Some(a)
+    def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] =
+      fa.flatMap(f)
+  }
+
+  val streamMonad: Monad[Stream] = new Monad[Stream] {
+    def unit[A](a: A): Stream[A] = Stream(a)
+    def flatMap[A, B](fa: Stream[A])(f: A => Stream[B]): Stream[B] =
+      fa.flatMap(f)
+  }
+
+  // def parserMonad[P[_]](p: Parsers[String, P]) = new Monad[P] {
+  //   def unit[A](a: A): P[A] = p.succeed(a)
+  //   def flatMap[A, B](fa: P[A])(f: A => P[B]): P[B] =
+  //     p.flatMap(fa)(f)
+  // }
+}
+
+object Main {
+  def main(args: Array[String]) =
+    println(Monad.optionMonad.filterM(List(1, 2, 3))(x => Some(x % 2 == 0)))
+    println(Monad.optionMonad.filterM0(List(1, 2, 3))(x => Some(x % 2 == 0)))
 }
