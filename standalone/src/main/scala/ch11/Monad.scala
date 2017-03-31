9767cba1177f9a65507cb554279f6e14c09ba033
Sergei Dolgov <dolgovs@gmail.com>
2017-03-31 09:24:57 +0200

    11.20

--- standalone/src/main/scala/ch11/Monad.scala
+++ standalone/src/main/scala/ch11/Monad.scala
@@ -114,6 +114,18 @@ case class Id[A](value: A) {
   def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
 
+case class Reader[R, A](run: R => A)
+
+object Reader {
+  def readerMonad[R] = new Monad[({type f[x] = Reader[R, x]})#f] {
+    def unit[A](a: A): Reader[R, A] = Reader(_ => a)
+    def flatMap[A, B](fa: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+      Reader(r => {
+        f(fa.run(r)).run(r)
+      })
+  }
+}
+
 object Main {
   def main(args: Array[String]) =
     println(Monad.optionMonad.filterM(List(1, 2, 3))(x => Some(x % 2 == 0)))
