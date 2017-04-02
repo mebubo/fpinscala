fc9f7363d60153379736f5149e098e0a31b37a42
Sergei Dolgov <dolgovs@gmail.com>
2017-04-02 23:11:11 +0200

    12.3

--- standalone/src/main/scala/ch12/Applicative.scala
+++ standalone/src/main/scala/ch12/Applicative.scala
@@ -25,4 +25,10 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((_, _))
+
+  def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(map(fa)(f.curried))(fb))(fc)
+
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
 }
