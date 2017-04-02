6c431140c6f9339fa5e2b63b292ad5f0e198b3c6
Sergei Dolgov <dolgovs@gmail.com>
2017-04-02 22:53:22 +0200

    12.2

--- standalone/src/main/scala/ch12/Applicative.scala
+++ standalone/src/main/scala/ch12/Applicative.scala
@@ -3,11 +3,16 @@ package ch12
 import ch11.Functor
 
 trait Applicative[F[_]] extends Functor[F] {
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
   def unit[A](a: A): F[A]
 
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
+
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fa, fab)((a, ab) => ab(a))
+
   def map[A, B](fa: F[A])(f: A => B): F[B] =
-    map2(fa, unit(()))((a, _) => f(a))
+    apply(unit(f))(fa)
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, flb) => map2(f(a), flb)(_ :: _))
