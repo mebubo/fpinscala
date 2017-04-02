90af547932f3531863f2e18815e46c10f3dcfdc5
Sergei Dolgov <dolgovs@gmail.com>
2017-04-02 22:44:05 +0200

    12.1

---
+++ standalone/src/main/scala/ch12/Applicative.scala
@@ -0,0 +1,23 @@
+package ch12
+
+import ch11.Functor
+
+trait Applicative[F[_]] extends Functor[F] {
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def unit[A](a: A): F[A]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    map2(fa, unit(()))((a, _) => f(a))
+
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, flb) => map2(f(a), flb)(_ :: _))
+
+  def sequence[A](as: List[F[A]]): F[List[A]] =
+    traverse(as)(x => x)
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+    sequence(List.fill(n)(fa))
+
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((_, _))
+}
