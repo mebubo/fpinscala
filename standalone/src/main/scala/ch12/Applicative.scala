2ba35c0490c5f290e4841f1b0decb447e959b86d
Sergei Dolgov <dolgovs@gmail.com>
2017-04-03 19:16:09 +0200

    12.6

--- standalone/src/main/scala/ch12/Applicative.scala
+++ standalone/src/main/scala/ch12/Applicative.scala
@@ -41,6 +41,24 @@ trait Applicative[F[_]] extends Functor[F] {
     }
 }
 
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E] = Vector()) extends Validation[E, Nothing]
+case class Success[A](a: A) extends Validation[Nothing, A]
+
+object Applicative {
+  def validationApplicative[E] = new Applicative[({type f[x] = Validation[E, x]})#f] {
+    def unit[A](a: A): Validation[E, A] = Success(a)
+    override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa, fb) match {
+      case (Success(a), Success(b)) => Success(f(a, b))
+      case (Failure(h1, t1), Failure(h2, t2)) => Failure(h1, t1 ++ Vector(h2) ++ t2)
+      case (e@Failure(_, _), _) => e
+      case (_, e@Failure(_, _)) => e
+    }
+
+  }
+}
+
 object Monad2 {
   def eitherMonad[E] = new Monad[({type f[x] = Either[E, x]})#f] {
     def unit[A](a: A): Either[E, A] = Right(a)
